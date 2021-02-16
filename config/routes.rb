3fda7288aa275c5c6f5288bac6647a4dc869650d
kodai.iwashita <back.together0607@gmail.com>
2021-02-16 08:49:31 +0000

    favorite

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,12 @@ Rails.application.routes.draw do
         member do
           get :followings
           get :followers
+          get :likes
         end
-        collection do
-          get :search
     end
-end
     
-    resources :microposts, only: [:create, :destroy]
+    resources :microposts, only: [:create, :destroy] do
+      resource :favorites, only: [:create, :destroy]
+    end
     resources :relationships, only: [:create, :destroy]
 end
